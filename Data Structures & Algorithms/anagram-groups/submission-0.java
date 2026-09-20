c81cd48ac4d5e0027ef6f8ee9170747226b98773
Shubham Raj <171572332+shuvmraj@users.noreply.github.com>
2026-09-20 20:42:20 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,17 @@
+//anagram easiest approach is sorting pr yaha qki list bnanana hai we'll have to see make a list as value in hashmap and there's everyhting the same
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> mp = new HashMap<>();
+        for(int i=0;i<strs.length;i++){
+            char[] arr = strs[i].toCharArray();
+            Arrays.sort(arr);
+            String sorted = new String(arr);
+
+            if(!mp.containsKey(sorted)){
+                mp.put(sorted,new ArrayList<>());
+            }
+            mp.get(sorted).add(strs[i]);
+        }
+        return new ArrayList<>(mp.values());
+    }
+}
